f52cc4e48b25a804c3e7bc54f3e3f9aab9c7bf4a
Daniel Johnson <navseeker7@gmail.com>
2015-09-23 08:46:34 -0700

    update coffee-script/register syntax

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-require('coffee-script/register');
+//require('coffee-script/register');
+var trans = require( 'coffee-script' );
+if ( trans.register )  trans.register();
+
+
 module.exports = require('./server').startServer;
